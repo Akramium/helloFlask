ee0fac83cd5288ba446bea7c97ae3f8740e4c16c
akramuim <o.akramuim@gmail.com>
2021-05-13 11:52:09 +0000

    python decorator

--- hello.py
+++ hello.py
@@ -3,6 +3,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return f'<b>{function()}</b>'
+
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return f'<em>{function()}</em>'
+
+    return wrapper
+
+
+def make_underline(function):
+    def wrapper():
+        return f'<u>{function()}</u>'
+
+    return wrapper
+
+
 @app.route('/')
 def hello_world():
     return '<h1 style="text-align:center">Hello, World!</h1>' \
@@ -11,6 +32,9 @@ def hello_world():
 
 
 @app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underline
 def say_bye():
     return 'Bye!'
 
