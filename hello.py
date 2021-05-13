c7b6e54e141e0e4236171cc458ffeea7ca071694
akramuim <o.akramuim@gmail.com>
2021-05-13 10:47:04 +0000

    render html

--- hello.py
+++ hello.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="text-align:center">Hello, World!</h1>' \
+           '<p>This is a paragraph</p>' \
+           '<img src="https://media.giphy.com/media/KAq5w47R9rmTuvWOWa/giphy.gif" width="200">'
 
 
 @app.route('/bye')
