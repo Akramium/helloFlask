d228a7b80cec01a23d1b196d18b6ebac6f36eb3f
akramuim <o.akramuim@gmail.com>
2021-05-13 10:16:34 +0000

    Learning

--- hello.py
+++ hello.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/bye')
+def say_bye():
+    return 'Bye!'
+
+
+@app.route('/username/<name>/<int:id_int>/<path:path>')
+def greet(name, id_int, path):
+    return f'Hello, {name}! You have an ID of: {id_int}, You path is: {path}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
